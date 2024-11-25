741bd1cb50640a6bae4c80a38ce1bbd290bbf923
Rasads7 <rafasilva1807@gmail.com>
2024-11-25 18:36:25 -0300

    2versao

--- teste.py
+++ teste.py
@@ -1,46 +1,73 @@
 import numpy as np
 
+# Matriz 4x4
+A = np.matrix([[0, 0, 1, 0],
+               [0, 0, 0, 1],
+               [1, 0, 0, 0],
+               [0, 1, 0, 0]])
 
-A = np.matrix([[4, 3],
-              [1, 2]])
-
+# Cálculo dos autovalores e autovetores
 autov_A = np.linalg.eig(A)
 
-#Apresenta tanto autovalores como autovetores
-print(autov_A)
-print('\n') 
-
-#Autovalores
-print(autov_A[0])
+# Apresenta tanto autovalores como autovetores
+print("Autovalores e Autovetores:\n", autov_A)
 print('\n')
 
-#Primeiro autovalor
-print(autov_A[0][0])
+# Autovalores
+print("Autovalores:\n", autov_A[0])
 print('\n')
 
-#Segundo autovalor
-print(autov_A[0][1])
+# Primeiro autovalor
+print("Primeiro autovalor:\n", autov_A[0][0])
 print('\n')
 
-#Autovetores
-print(autov_A[1])
+# Segundo autovalor
+print("Segundo autovalor:\n", autov_A[0][1])
 print('\n')
 
-#Primeiro autovetor
-print(autov_A[1][:,0])
+# Terceiro autovalor
+print("Terceiro autovalor:\n", autov_A[0][2])
 print('\n')
 
-#Segundo autovetor
-print(autov_A[1][:,1])
+# Quarto autovalor
+print("Quarto autovalor:\n", autov_A[0][3])
 print('\n')
 
-#Primeiro autovetor normalizado
-print(autov_A[1][:,0]/autov_A[1][1,0])
+# Autovetores
+print("Autovetores:\n", autov_A[1])
 print('\n')
 
-#Segundo autovetor normalizado
-print(autov_A[1][:,1]/autov_A[1][1,1])
+# Primeiro autovetor
+print("Primeiro autovetor:\n", autov_A[1][:, 0])
 print('\n')
 
-#Matriz Diagonal
-print(np.diag(autov_A[0]))
+# Segundo autovetor
+print("Segundo autovetor:\n", autov_A[1][:, 1])
+print('\n')
+
+# Terceiro autovetor
+print("Terceiro autovetor:\n", autov_A[1][:, 2])
+print('\n')
+
+# Quarto autovetor
+print("Quarto autovetor:\n", autov_A[1][:, 3])
+print('\n')
+
+'''# Primeiro autovetor normalizado
+print("Primeiro autovetor normalizado:\n", autov_A[1][:, 0] / autov_A[1][1, 0])
+print('\n')
+
+# Segundo autovetor normalizado
+print("Segundo autovetor normalizado:\n", autov_A[1][:, 1] / autov_A[1][1, 1])
+print('\n')'''
+
+# Terceiro autovetor normalizado
+print("Primeiro autovetor normalizado:\n", autov_A[1][:, 2] / autov_A[1][1, 2])
+print('\n')
+
+# Quarto autovetor normalizado
+print("Segundo autovetor normalizado:\n", autov_A[1][:, 3] / autov_A[1][1, 3])
+print('\n')
+
+# Matriz Diagonal
+print("Matriz Diagonal (autovalores):\n", np.diag(autov_A[0]))
